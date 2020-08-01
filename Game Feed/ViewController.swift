e1244470891ef7c25b2eff70791a6d048ef30d62
M Nazar Alwi <alwinazar75@gmail.com>
2020-08-01 15:58:15 +0700

    Initial Commit

---
+++ Game Feed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Game Feed
+//
+//  Created by M Nazar Alwi on 01/08/20.
+//  Copyright © 2020 Dicoding Indonesia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
