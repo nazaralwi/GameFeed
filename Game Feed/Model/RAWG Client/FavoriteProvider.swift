ca87476c0fabeeaab64095c931c3421a1e2bce7d
M Nazar Alwi <alwinazar75@gmail.com>
2020-10-19 11:53:47 +0700

    Add getFavorite function to get each favorite game

--- Game Feed/Model/RAWG Client/FavoriteProvider.swift
+++ Game Feed/Model/RAWG Client/FavoriteProvider.swift
@@ -57,4 +57,26 @@ class FavoriteProvider {
             }
         }
     }
+    
+    func getFavorite(_ id: Int, completion: @escaping(_ members: FavoriteModel) -> ()) {
+        let taskContext = newTaskContext()
+        taskContext.perform {
+            let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Favorite")
+            fetchRequest.fetchLimit = 1
+            fetchRequest.predicate = NSPredicate(format: "id == \(id)")
+            do {
+                if let result = try taskContext.fetch(fetchRequest).first{
+                    let favorite =  FavoriteModel(id: result.value(forKeyPath: "id") as? Int32,
+                                                  name: result.value(forKeyPath: "name") as? String,
+                                                  released: result.value(forKeyPath: "released") as? String,
+                                                  rating: result.value(forKeyPath: "rating") as? Double,
+                                                  backgroundImage: result.value(forKeyPath: "backgroundImage") as? String,
+                                                  genres: result.value(forKeyPath: "genres") as? String)
+                    completion(favorite)
+                }
+            } catch let error as NSError {
+                print("Could not fetch. \(error), \(error.userInfo)")
+            }
+        }
+    }
 }
