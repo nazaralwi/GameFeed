97f417e7ede11a17fea6fb030a5b5fd81de406e3
M Nazar Alwi <alwinazar75@gmail.com>
2020-10-19 11:52:48 +0700

    Add getAllFavorites function to get all favorite data from persistance

--- Game Feed/Model/RAWG Client/FavoriteProvider.swift
+++ Game Feed/Model/RAWG Client/FavoriteProvider.swift
@@ -33,4 +33,28 @@ class FavoriteProvider {
         taskContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
         return taskContext
     }
+    
+    func getAllFavorites(completion: @escaping(_ members: [FavoriteModel]) -> ()) {
+        let taskContext = newTaskContext()
+        taskContext.perform {
+            let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Favorite")
+            do {
+                let results = try taskContext.fetch(fetchRequest)
+                var favorites: [FavoriteModel] = []
+                for result in results {
+                    let favorite = FavoriteModel(id: result.value(forKeyPath: "id") as? Int32,
+                                                 name: result.value(forKeyPath: "name") as? String,
+                                                 released: result.value(forKeyPath: "released") as? String,
+                                                 rating: result.value(forKeyPath: "rating") as? Double,
+                                                 backgroundImage: result.value(forKeyPath: "backgroundImage") as? String,
+                                                 genres: result.value(forKeyPath: "genres") as? String)
+                    
+                    favorites.append(favorite)
+                }
+                completion(favorites)
+            } catch let error as NSError {
+                print("Could not fetch. \(error), \(error.userInfo)")
+            }
+        }
+    }
 }
