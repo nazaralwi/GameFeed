552f7b847bd4d6626d24c3b3e40ab651514b2a9b
M Nazar Alwi <alwinazar75@gmail.com>
2020-10-19 11:50:44 +0700

    Add FavoriteModel struct

---
+++ Game Feed/Model/RAWG Client/FavoriteModel.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteModel.swift
+//  Game Feed
+//
+//  Created by M Nazar Alwi on 17/10/20.
+//  Copyright © 2020 Dicoding Indonesia. All rights reserved.
+//
+
+import UIKit
+
+struct FavoriteModel {
+    var id: Int32?
+    var name: String?
+    var released: String?
+    var rating: Double?
+    var backgroundImage: String?
+    var genres: String?
+}
