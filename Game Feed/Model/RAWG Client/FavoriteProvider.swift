511107a29b6cc94687943b525e55bde56604feb5
M Nazar Alwi <alwinazar75@gmail.com>
2020-10-19 11:49:32 +0700

    Add Provider Class with Persistent Container Variable

---
+++ Game Feed/Model/RAWG Client/FavoriteProvider.swift
@@ -0,0 +1,30 @@
+//
+//  FavoriteProvider.swift
+//  Game Feed
+//
+//  Created by M Nazar Alwi on 17/10/20.
+//  Copyright © 2020 Dicoding Indonesia. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class FavoriteProvider {
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "UserFavorites")
+        
+        container.loadPersistentStores { storeDesription, error in
+            guard error == nil else {
+                fatalError("Unresolved error \(error!)")
+            }
+        }
+        container.viewContext.automaticallyMergesChangesFromParent = false
+        container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+        container.viewContext.shouldDeleteInaccessibleFaults = true
+        container.viewContext.undoManager = nil
+        
+        return container
+    }()
+    
+    
+}
