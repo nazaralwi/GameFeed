111f6a3d37c551bdd36281478c1051616b185ca3
Nazar Alwi <alwinazar75@gmail.com>
2025-09-18 13:56:50 +0700

    Use programmatic UIKit on Profile and Edit Profile View

--- GameFeed/Presentation/Scenes/MyProfile/MyProfileViewController.swift
+++ GameFeed/Presentation/Scenes/MyProfile/MyProfileViewController.swift
@@ -1,11 +1,76 @@
 import UIKit
 
 final class MyProfileViewController: UIViewController {
-    @IBOutlet var nameLabel: UILabel!
-    @IBOutlet var companyLabel: UILabel!
-    @IBOutlet var emailLabel: UILabel!
+    private let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "myphoto")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 22, weight: .bold)
+        label.textColor = .label
+        label.textAlignment = .center
+        return label
+    }()
+
+    private let companyLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 16)
+        label.textColor = .secondaryLabel
+        label.textAlignment = .center
+        return label
+    }()
+
+    private let emailLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 16)
+        label.textColor = .secondaryLabel
+        label.textAlignment = .center
+        return label
+    }()
 
     var viewModel: MyProfileViewModel?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+
+        [profileImageView, nameLabel, companyLabel, emailLabel].forEach {
+            view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+
+        NSLayoutConstraint.activate([
+            profileImageView.widthAnchor.constraint(equalToConstant: 150),
+            profileImageView.heightAnchor.constraint(equalToConstant: 150),
+
+            profileImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
+            profileImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+
+            nameLabel.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 24),
+            nameLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 24),
+            nameLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -24),
+
+            companyLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 12),
+            companyLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
+            companyLabel.trailingAnchor.constraint(equalTo: nameLabel.trailingAnchor),
+
+            emailLabel.topAnchor.constraint(equalTo: companyLabel.bottomAnchor, constant: 8),
+            emailLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
+            emailLabel.trailingAnchor.constraint(equalTo: nameLabel.trailingAnchor)
+        ])
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        profileImageView.layer.cornerRadius = profileImageView.frame.height / 2
+    }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
--- GameFeed/Presentation/Scenes/UpdateProfile/UpdateViewController.swift
+++ GameFeed/Presentation/Scenes/UpdateProfile/UpdateViewController.swift
@@ -1,18 +1,118 @@
 import UIKit
 
 final class UpdateViewController: UIViewController {
-    @IBOutlet var photo: UIImageView!
-    @IBOutlet var nameTextField: UITextField!
-    @IBOutlet var companyTextField: UITextField!
-    @IBOutlet var emailTextField: UITextField!
+    private let photoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "myphoto")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Name"
+        label.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+        label.textColor = .secondaryLabel
+        return label
+    }()
+
+    let nameTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.font = UIFont.systemFont(ofSize: 16)
+        return textField
+    }()
+
+    let companyLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Company"
+        label.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+        label.textColor = .secondaryLabel
+        return label
+    }()
+
+    let companyTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.font = UIFont.systemFont(ofSize: 16)
+        return textField
+    }()
+
+    let emailLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Email"
+        label.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+        label.textColor = .secondaryLabel
+        return label
+    }()
+
+    let emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.font = UIFont.systemFont(ofSize: 16)
+        textField.keyboardType = .emailAddress
+        return textField
+    }()
 
     var viewModel: MyProfileViewModel?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        view.backgroundColor = .systemBackground
+        
+        [photoImageView,
+         nameLabel, nameTextField,
+         companyLabel, companyTextField,
+         emailLabel, emailTextField].forEach {
+            view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+
+        NSLayoutConstraint.activate([
+            // Photto
+            photoImageView.widthAnchor.constraint(equalToConstant: 150),
+            photoImageView.heightAnchor.constraint(equalToConstant: 150),
+            photoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
+            photoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+
+            // Name
+            nameLabel.topAnchor.constraint(equalTo: photoImageView.bottomAnchor, constant: 32),
+            nameLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 24),
+
+            nameTextField.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
+            nameTextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 24),
+            nameTextField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -24),
+            nameTextField.heightAnchor.constraint(equalToConstant: 44),
+
+            // Company
+            companyLabel.topAnchor.constraint(equalTo: nameTextField.bottomAnchor, constant: 24),
+            companyLabel.leadingAnchor.constraint(equalTo: nameTextField.leadingAnchor),
+
+            companyTextField.topAnchor.constraint(equalTo: companyLabel.bottomAnchor, constant: 8),
+            companyTextField.leadingAnchor.constraint(equalTo: nameTextField.leadingAnchor),
+            companyTextField.trailingAnchor.constraint(equalTo: nameTextField.trailingAnchor),
+            companyTextField.heightAnchor.constraint(equalToConstant: 44),
+
+            // Email
+            emailLabel.topAnchor.constraint(equalTo: companyTextField.bottomAnchor, constant: 24),
+            emailLabel.leadingAnchor.constraint(equalTo: nameTextField.leadingAnchor),
+
+            emailTextField.topAnchor.constraint(equalTo: emailLabel.bottomAnchor, constant: 8),
+            emailTextField.leadingAnchor.constraint(equalTo: nameTextField.leadingAnchor),
+            emailTextField.trailingAnchor.constraint(equalTo: nameTextField.trailingAnchor),
+            emailTextField.heightAnchor.constraint(equalToConstant: 44)
+        ])
+
         let tapGesture = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
         view.addGestureRecognizer(tapGesture)
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        photoImageView.layer.cornerRadius = photoImageView.frame.width / 2
     }
 
     override func viewWillAppear(_ animated: Bool) {
